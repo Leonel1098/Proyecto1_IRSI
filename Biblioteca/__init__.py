65869d4568694386ecbc3c765b3ff506dbd83cb7
Leonel <gonzalezleonel1098@gmail.com>
2024-07-23 23:32:50 -0600

    Ventana Prestamos

--- Biblioteca/__init__.py
+++ Biblioteca/__init__.py
@@ -34,7 +34,7 @@ class Interfaz:
         button_usuarios.config(bg = "black")
         button_usuarios.place(x = 195,y = 250, width= 100, height  = 40 )
 
-        button_prestamo = Button(p_Frame, text="Prestamos",font=("Modern", 16), foreground = "white", highlightthickness=2)
+        button_prestamo = Button(p_Frame, text="Prestamos",font=("Modern", 16), command= self.ventana_Prestamo, foreground = "white", highlightthickness=2)
         button_prestamo.pack()
         button_prestamo.config(bg = "black")
         button_prestamo.place(x =295,y= 250, width= 100, height  = 40)
@@ -124,7 +124,7 @@ class Interfaz:
         button_buscar.config(bg="black")
         button_buscar.place(x=370, y=220, width=110, height=20)
 
-        button_regresar = Button(libros_Frame, text="Regresar", command= self.regresar, font=("Modern", 12), foreground="white", highlightthickness=2)
+        button_regresar = Button(libros_Frame, text="Regresar", command= lambda: self.regresar(self.ventana_libros), font=("Modern", 12), foreground="white", highlightthickness=2)
         button_regresar.pack()
         button_regresar.config(bg="black")
         button_regresar.place(x=370, y=265, width=110, height=20)
@@ -135,7 +135,7 @@ class Interfaz:
 
         self.ventana_principal.iconify()
         self.ventana_usuarios = tk.Toplevel()
-        self.ventana_usuarios.title("Gestionando Libros")
+        self.ventana_usuarios.title("Gestionando Usuarios")
         self.ventana_usuarios.geometry("%dx%d+%d+%d" % (500, 300, 450, 220))
         self.ventana_usuarios.resizable(0, 0)
 
@@ -144,7 +144,7 @@ class Interfaz:
         usuarios_Frame.place(width="500", height="300")
         usuarios_Frame.config(bg="black")
 
-        label_nombre = Label(usuarios_Frame, text="Titulo del Libro", font=("Modern", 12), foreground="white")
+        label_nombre = Label(usuarios_Frame, text="Nombre del Usuario", font=("Modern", 12), foreground="white")
         label_nombre.pack()
         label_nombre.config(bg="black")
         label_nombre.place(x=20, y=40, width=200, height=20)
@@ -153,37 +153,90 @@ class Interfaz:
         entrada_nombre.pack()
         entrada_nombre.place(x = 200, y = 40, width = 160, height = 20)
 
-        label_autor = Label(usuarios_Frame, text="Nombre del Autor", font=("Modern", 12), foreground="white")
-        label_autor.pack()
-        label_autor.config(bg="black")
-        label_autor.place(x=20, y=70, width=200, height=20)
+        label_id = Label(usuarios_Frame, text="ID Usuario", font=("Modern", 12), foreground="white")
+        label_id.pack()
+        label_id.config(bg="black")
+        label_id.place(x=20, y=70, width=200, height=20)
 
-        entrada_autor = tk.Entry(usuarios_Frame)
-        entrada_autor.pack()
-        entrada_autor.place(x= 200, y=70, width=160, height=20)
+        entrada_id = tk.Entry(usuarios_Frame)
+        entrada_id.pack()
+        entrada_id.place(x= 200, y=70, width=160, height=20)
 
-        label_isbn = Label(usuarios_Frame, text="Ingrese el ISBN del libro", font=("Modern", 12), foreground="white")
-        label_isbn.pack()
-        label_isbn.config(bg="black")
-        label_isbn.place(x=20, y=100, width=200, height=20)
+        button_registrar_usuario = Button(usuarios_Frame, text="Registrar Usuario", font=("Modern", 12), foreground="white",highlightthickness=2)
+        button_registrar_usuario.pack()
+        button_registrar_usuario.config(bg="black")
+        button_registrar_usuario.place(x=230, y=100, width=110, height=20)
 
-        entrada_isbn = tk.Entry(usuarios_Frame)
-        entrada_isbn.pack()
-        entrada_isbn.place(x=200, y=100, width=160, height=20)
+        label_eliminar_usuario = Label(usuarios_Frame, text="Ingrese el ID del Usuario", font=("Modern", 12), foreground="white")
+        label_eliminar_usuario.pack()
+        label_eliminar_usuario.config(bg="black")
+        label_eliminar_usuario.place(x=20, y=180, width=200, height=20)
 
-        button_guardar = Button(usuarios_Frame, text="Guardar Libro", font=("Modern", 12), foreground="white",highlightthickness=2)
-        button_guardar.pack()
-        button_guardar.config(bg="black")
-        button_guardar.place(x=370, y=100, width=110, height=20)
+        entrada_eliminar_usuario = tk.Entry(usuarios_Frame)
+        entrada_eliminar_usuario.pack()
+        entrada_eliminar_usuario.place(x=200, y=180, width=160, height=20)
 
+        button_eliminar_usuario = Button(usuarios_Frame, text="Eliminar Usuario", font=("Modern", 12), foreground="white",highlightthickness=2)
+        button_eliminar_usuario.pack()
+        button_eliminar_usuario.config(bg="black")
+        button_eliminar_usuario.place(x=370, y=180, width=110, height=20)
 
+        button_regresar = Button(usuarios_Frame, text="Regresar", command= lambda: self.regresar(self.ventana_usuarios), font=("Modern", 12), foreground="white", highlightthickness=2)
+        button_regresar.pack()
+        button_regresar.config(bg="black")
+        button_regresar.place(x=370, y=255, width=110, height=20)
         self.ventana_usuarios.mainloop()
 
 
+    def ventana_Prestamo(self):
+        self.ventana_principal.iconify()
+        self.ventana_prestamos = tk.Toplevel()
+        self.ventana_prestamos.title("Gestionando Usuarios")
+        self.ventana_prestamos.geometry("%dx%d+%d+%d" % (500, 300, 450, 220))
+        self.ventana_prestamos.resizable(0, 0)
 
-    def regresar(self):
+        prestamos_Frame = Frame(self.ventana_prestamos)
+        prestamos_Frame.pack(side="top")
+        prestamos_Frame.place(width="500", height="300")
+        prestamos_Frame.config(bg="black")
+
+        label_isbn = Label(prestamos_Frame, text="Ingrese el ISBN del libro", font=("Modern", 12), foreground="white")
+        label_isbn.pack()
+        label_isbn.config(bg="black")
+        label_isbn.place(x=20, y=50, width=200, height=20)
+
+        entrada_isbn = tk.Entry(prestamos_Frame)
+        entrada_isbn.pack()
+        entrada_isbn.place(x=200, y=50, width=160, height=20)
+
+        label_id_usuario = Label(prestamos_Frame, text="Ingrese el ID del Usuario", font=("Modern", 12), foreground="white")
+        label_id_usuario.pack()
+        label_id_usuario.config(bg="black")
+        label_id_usuario.place(x=20, y=90, width=200, height=20)
+
+        entrada_id_usuario = tk.Entry(prestamos_Frame)
+        entrada_id_usuario.pack()
+        entrada_id_usuario.place(x=200, y=90, width=160, height=20)
+
+        button_prestar = Button(prestamos_Frame, text="Prestar Libro", font=("Modern", 12), foreground="white", highlightthickness=2)
+        button_prestar.pack()
+        button_prestar.config(bg="black")
+        button_prestar.place(x=180, y=140, width=110, height=20)
+
+        button_devolver = Button(prestamos_Frame, text="Devolver Libro", font=("Modern", 12), foreground="white", highlightthickness=2)
+        button_devolver.pack()
+        button_devolver.config(bg="black")
+        button_devolver.place(x=300, y=140, width=110, height=20)
+
+        button_regresar = Button(prestamos_Frame, text="Regresar", command= lambda: self.regresar(self.ventana_prestamos), font=("Modern", 12), foreground="white", highlightthickness=2)
+        button_regresar.pack()
+        button_regresar.config(bg="black")
+        button_regresar.place(x=370, y=265, width=110, height=20)
+
+    def regresar(self,ventana):
+        
         self.ventana_principal.deiconify()
-        self.ventana_libros.destroy()
+        ventana.destroy()
 
 
 if __name__ == "__main__":
